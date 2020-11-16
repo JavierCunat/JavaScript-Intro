74a2eaad94a1e81847fe0b4062d08d20e16210cb
javier <jcunat22517@columbushs.com>
2020-11-16 05:18:47 +0000

    Video 116 Completed

--- script.js
+++ script.js
@@ -74,3 +74,35 @@ var c;
 let firstNumber = prompt("Enter first number");
 let secondNumber = prompt("Enter second number");
 let sum = alert(Number(firstNumber) + Number(secondNumber));
+
+
+//                                 Video 116 Udemy Exercises
+
+// Make a keyless car!
+// This car will only let you drive if you are over 18. Make it do the following:
+
+
+//using prompt() and alert(), ask a user for their age.
+// IF they say they are below 18, respond with:
+// "Sorry, you are too young to drive this car. Powering off"
+
+// IF they say they are 18, respond with:
+// "Congratulations on your first year of driving. Enjoy the ride!"
+
+// IF they say they are over 18, respond with:
+// "Powering On. Enjoy the ride!"
+
+let ageCalculator = prompt("What is your age?");
+
+if (Number(ageCalculator) < 18) {
+    alert("Sorry you are too young to drive this car, powering off.");
+}
+else if (Number(ageCalculator) === 18) {
+    alert("Congragulations on your first year driving. Enjoy the ride!");
+}
+else if (Number(ageCalculator) > 18) {
+    alert("Powering on. Enjoy your ride");
+}
+else if (Number(ageCalculator) >= 80){
+    alert("You shouldnt be driving at this age.");
+}
